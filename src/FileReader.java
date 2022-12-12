a8e34e62b853d387d81bbc8bae722bededdee43e
ashutosh2406 <119935143+ashutosh2406@users.noreply.github.com>
2022-12-12 12:36:32 +0530

    changed parameters

--- src/FileReader.java
+++ src/FileReader.java
@@ -10,14 +10,14 @@ class FileReader {
 
     public static boolean isFileExists() {
         if (FileSearchApplication.filepath.endsWith("txt") || FileSearchApplication.filepath.endsWith("json")) {
-            readingFileContent(FileSearchApplication.filepath);
+            readingFileContent();
             return true;
         } else {
             return false;
         }
     }
 
-    public static void readingFileContent(String filePath) {
+    public static void readingFileContent() {
         try {
             List<String> allLines = Files.readAllLines(Paths.get(FileSearchApplication.filepath));
             for (String temporaryMemoryToFileContent : allLines) {
